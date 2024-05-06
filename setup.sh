ceacbc11d81ec284bbec9be7f66a666a2d308a5f
Juan Pablo Nahuelpán <nahuelpan.ide@gmail.com>
2024-05-06 16:57:38 -0400

    fix: delete sudo comands in mkdir

--- setup.sh
+++ setup.sh
@@ -2,14 +2,14 @@
 sudo apt install awesome xinit rofi kitty ranger -y
 
 # config startx for awesomewm:
-sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
-sudo echo -e "\n# setup for awesomewm
+cp /etc/X11/xinit/xinitrc ~/.xinitrc
+echo -e "\n# setup for awesomewm
 if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
     exec startx
 fi\n" >> ~/.profile
 
 # awesome config:
-sudo mkdir -p ~/.config/awesome/
+mkdir -p ~/.config/awesome/
 cp rc.lua ~/.config/awesome/
 
 # rofi conf:

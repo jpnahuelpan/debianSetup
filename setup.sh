a73ccbe417f5238ba6b1e400eb50f922c3229f64
Juan Pablo Nahuelpán <nahuelpan.ide@gmail.com>
2024-05-06 16:28:56 -0400

    fix: comenting rofi commands

--- setup.sh
+++ setup.sh
@@ -3,18 +3,18 @@ sudo apt install awesome xinit rofi kitty ranger -y
 
 # config startx for awesomewm:
 sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
-echo -e '\n# setup for awesomewm
+sudo echo -e '\n# setup for awesomewm
 if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
     exec startx
-fi\n' >> .profile.sh
+fi\n' >> ~/.profile.sh
 
 # awesome config:
 sudo mkdir -p ~/.config/awesome
 cp rc.lua ~/.config/awesome/rc.lua
 
 # rofi conf:
-sudo mkdir -p ~/.config/rofi
-rofi -dump-config > ~/.config/rofi/config.rasi
+# sudo mkdir -p ~/.config/rofi
+# rofi -dump-config > ~/.config/rofi/config.rasi
 
 # kitty config:
 sudo mkdir -p ~/.config/kitty

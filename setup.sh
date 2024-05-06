1b55ecfe4af0ac1e2bff96c6b102bfb1fc5e141e
Juan Pablo Nahuelpán <nahuelpan.ide@gmail.com>
2024-05-06 16:20:41 -0400

    fix: mkdir commands

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@ apt install awesome xinit rofi kitty ranger -y
 
 # config startx for awesomewm:
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
-echo -e "\n# setup for awesomewm
-if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" == 1]; then
+echo -e '\n# setup for awesomewm
+if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
     exec startx
-fi\n" >> .profile.sh
+fi\n' >> .profile.sh
 
 # awesome config:
-makdir -p ~/.config/awesome
+mkdir -p ~/.config/awesome
 cp rc.lua ~/.config/awesome/rc.lua
 
 # rofi conf:

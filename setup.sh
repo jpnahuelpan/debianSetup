f06f463d35f1aeef61b66e5ea700e35a1e0b4fb4
Juan Pablo Nahuelpán <nahuelpan.ide@gmail.com>
2024-05-06 15:15:25 -0400

    mod: adding kitty conf

--- setup.sh
+++ setup.sh
@@ -15,3 +15,9 @@ cp rc.lua ~/.config/awesome/rc.lua
 # rofi conf:
 mkdir -p ~/.config/rofi
 rofi -dump-config > ~/.config/rofi/config.rasi
+
+# kitty config:
+mkdir -p ~/.config/kitty
+wget https://raw.githubusercontent.com/kovidgoyal/kitty-themes/master/themes/GitHub_Dark.conf
+cp GitHub_Dark.conf ~/.config/kitty/kitty.conf
+rm GitHub_Dark.conf

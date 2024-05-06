17107ec3fa40a744e28f911155d4e82c8bac2035
Juan Pablo Nahuelpán <nahuelpan.ide@gmail.com>
2024-05-06 16:43:37 -0400

    fix: fixing errors

--- setup.sh
+++ setup.sh
@@ -3,21 +3,21 @@ sudo apt install awesome xinit rofi kitty ranger -y
 
 # config startx for awesomewm:
 sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
-sudo echo -e '\n# setup for awesomewm
+sudo echo -e "\n# setup for awesomewm
 if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
     exec startx
-fi\n' >> ~/.profile.sh
+fi\n" >> ~/.profile
 
 # awesome config:
-sudo mkdir -p ~/.config/awesome
-cp rc.lua ~/.config/awesome/rc.lua
+sudo mkdir -p ~/.config/awesome/
+cp rc.lua ~/.config/awesome/
 
 # rofi conf:
 # sudo mkdir -p ~/.config/rofi
 # rofi -dump-config > ~/.config/rofi/config.rasi
 
 # kitty config:
-sudo mkdir -p ~/.config/kitty
+mkdir -p ~/.config/kitty/
 wget https://raw.githubusercontent.com/kovidgoyal/kitty-themes/master/themes/GitHub_Dark.conf
-cp GitHub_Dark.conf ~/.config/kitty/kitty.conf
-rm GitHub_Dark.conf
+mv GitHub_Dark.conf ~/.config/kitty/kitty.conf
+# rm GitHub_Dark.conf

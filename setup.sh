23a10c7463c51857d55e6eb02877d2f801bce1d1
Juan Pablo Nahuelpán <nahuelpan.ide@gmail.com>
2024-06-10 15:07:56 -0400

    update: setup.sh
    Adding kitty last version installation and make nautilus file manager over ranger.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # principal packages:
-sudo apt install awesome xinit rofi kitty ranger -y
+sudo apt install awesome xinit rofi nautilus -y
 
 # config startx for awesomewm:
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
@@ -13,7 +13,12 @@ mkdir -p ~/.config/awesome/
 cp rc.lua ~/.config/awesome/
 
 
-# kitty config:
-mkdir -p ~/.config/kitty/
-wget https://raw.githubusercontent.com/kovidgoyal/kitty-themes/master/themes/GitHub_Dark.conf
-mv GitHub_Dark.conf ~/.config/kitty/kitty.conf
+# kitty install:
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+echo -e '\n# kitty bin
+export PATH="$PATH:$HOME/.local/kitty.app/bin\n' >> ~/.profile
+cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
+sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+echo 'kitty.desktop' > ~/.config/xdg-terminals.list

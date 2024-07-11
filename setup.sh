fcab9813a13ff378c3467c71ac23f1a11a9c0872
Juan Pablo Nahuelpán <nahuelpan.ide@gmail.com>
2024-07-11 14:16:55 -0400

    update: adding ps1 config

--- setup.sh
+++ setup.sh
@@ -8,23 +8,11 @@ export PATH="$PATH:/sbin:/usr/sbin"\n' >> ~/.profile
 # install helix 24.03:
 curl -L -O https://github.com/helix-editor/helix/releases/download/24.03/helix-24.03-x86_64-linux.tar.xz
 mkdir -p ~/.local/helix-24.03/bin
-tar -xJvf helix-24.03-x86_64-linux.tar.xz
+tar -xJvf helix*.tar.xz
 mv helix-24.03-x86_64-linux/* ~/.local/helix-24.03/bin/
-# rm -rf helix-24.03-x86_64-linux
+mv helix/config.toml ~/.config/helix/
 echo -e '\n# helix bin
 export PATH="$PATH:$HOME/.local/helix-24.03/bin"' >> ~/.profile
-
-# config startx for awesomewm:
-cp /etc/X11/xinit/xinitrc ~/.xinitrc
-echo -e '\n# setup for awesomewm
-if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
-    exec startx
-fi\n' >> ~/.profile
-
-# awesome config:
-mkdir -p ~/.config/awesome/
-cp rc.lua ~/.config/awesome/
-
 
 # kitty install:
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
@@ -40,11 +28,20 @@ echo 'kitty.desktop' > ~/.config/xdg-terminals.list
 # install zellij
 curl -L -O https://github.com/zellij-org/zellij/releases/download/v0.40.1/zellij-x86_64-unknown-linux-musl.tar.gz
 tar -xvf zellij*.tar.gz
-# rm zellij*.tar.gz
-chmod +x zellij
+#chmod +x zellij
 mv zellij /usr/local/bin/zellij
 
 # install chrome
 curl -L -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install ./google-chrome-stable_current_amd64.deb
-# rm google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+# config startx for awesomewm:
+cp /etc/X11/xinit/xinitrc ~/.xinitrc
+echo -e '\n# setup for awesomewm
+if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
+    exec startx
+fi\n' >> ~/.profile
+
+# awesome config:
+mkdir -p ~/.config/awesome/
+cp rc.lua ~/.config/awesome/
